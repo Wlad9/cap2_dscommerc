25d1f38bd754aef4c0722c0a8a9fbfcf84ca0835
wlad9 <zav8b9@gmail.com>
2023-07-19 16:15:18 -0300

    Criado porjeto - Entities: User - Order - OrderStatus

---
+++ src/main/java/com/cap2/dscommerce/DscommerceApplication.java
@@ -0,0 +1,13 @@
+package com.cap2.dscommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DscommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DscommerceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/cap2/dscommerce/entities/User.java
@@ -0,0 +1,88 @@
+package com.cap2.dscommerce.entities;
+
+import jakarta.persistence.*;
+
+import java.security.PrivilegedExceptionAction;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "tb_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String phone;
+    private LocalDate birthDay;
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
+
+    public User() {
+    }
+
+    public User(Long id, String name, String email, String phone, LocalDate birthDay) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.birthDay = birthDay;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public LocalDate getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(LocalDate birthDay) {
+        this.birthDay = birthDay;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", phone='" + phone + '\'' +
+                ", birthDay=" + birthDay +
+                '}';
+    }
+}
