fc6e43a177617c061c1527785d560b5b4f9607de
wlad9 <zav8b9@gmail.com>
2023-07-23 15:09:34 -0300

    Atualizada entity OrderItem. Seeding de dados ainda com erro

--- src/main/java/com/cap2/dscommerce/entities/OrderItem.java
+++ src/main/java/com/cap2/dscommerce/entities/OrderItem.java
@@ -5,12 +5,16 @@ import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
+import java.util.Objects;
+
 
 @Entity
 @Table(name = "tb_order_item")
 public class OrderItem {
+
     @EmbeddedId
     private OrderItemPK id = new OrderItemPK();
+
     private Integer quantity;
     private Double price;
 
@@ -22,6 +26,22 @@ public class OrderItem {
         id.setProduct(product);
         this.quantity = quantity;
         this.price = price;
+    }
+
+    public Order getOrder() {
+        return id.getOrder();
+    }
+
+    public void setOrder(Order order) {
+        id.setOrder(order);
+    }
+
+    public Product getProduct() {
+        return id.getProduct();
+    }
+
+    public void setProduct(Product product) {
+        id.setProduct(product);
     }
 
     public Integer getQuantity() {
@@ -40,19 +60,18 @@ public class OrderItem {
         this.price = price;
     }
 
-    public void setOrder(Order order) {
-        id.setOrder(order);
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        OrderItem orderItem = (OrderItem) o;
+
+        return Objects.equals(id, orderItem.id);
     }
 
-    public Order getOrder() {
-        return id.getOrder();
-    }
-
-    public Product getProduct() {
-        return id.getProduct();
-    }
-
-    public void setProduct(Product product) {
-        id.setProduct(product);
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
     }
 }
