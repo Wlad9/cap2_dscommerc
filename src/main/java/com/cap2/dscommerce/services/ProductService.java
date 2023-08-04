f891fb81f1e5c7825567d8d65222926a61b43a05
wlad9 <zav8b9@gmail.com>
2023-08-04 16:37:31 -0300

    Erro. Não captura a exceção EntityNotFoundException e

--- src/main/java/com/cap2/dscommerce/services/ProductService.java
+++ src/main/java/com/cap2/dscommerce/services/ProductService.java
@@ -4,6 +4,7 @@ import com.cap2.dscommerce.dto.ProductDTO;
 import com.cap2.dscommerce.entities.Product;
 import com.cap2.dscommerce.repositories.ProductRepository;
 import com.cap2.dscommerce.services.exceptions.ResurceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -19,8 +20,8 @@ public class ProductService {
     private ProductRepository repository;
 
     @Transactional(readOnly = true)
-    public ProductDTO findBayId(Long id){
-        Product product = repository.findById(id).orElseThrow(()-> new ResurceNotFoundException("Recurso não encontrado"));
+    public ProductDTO findBayId(Long id) {
+        Product product = repository.findById(id).orElseThrow(() -> new ResurceNotFoundException("Recurso não encontrado"));
         return new ProductDTO(product);
     }
 
@@ -55,13 +56,18 @@ public class ProductService {
 
     @Transactional
     public ProductDTO update(Long id, ProductDTO dto) {
-        Product entity = repository.getReferenceById(id);
-        copyDtoToEntity(dto, entity);
-        entity = repository.save(entity);
-        return new ProductDTO(entity);
+        try {
+            Product entity = repository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = repository.save(entity);
+            return new ProductDTO(entity);
+        } catch (EntityNotFoundException e) {
+            throw new EntityNotFoundException("Recurso não encontrado");
+        }
     }
+
     @Transactional
-    public void delete(Long id){
+    public void delete(Long id) {
         repository.deleteById(id);
     }
 
